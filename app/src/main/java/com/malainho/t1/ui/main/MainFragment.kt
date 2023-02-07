f8a3f235e96dcd15c3905581b0fd782a3bad7932
cesar.malainho <cesar.malainho@alten.es>
2023-02-07 13:58:42 +0100

    Commit para remover o ViewModule

--- app/src/main/java/com/malainho/t1/ui/main/MainFragment.kt
+++ app/src/main/java/com/malainho/t1/ui/main/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.malainho.t1.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,14 +15,6 @@ class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
-    }
-
-    // private lateinit var viewModel: MainViewModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
     override fun onCreateView(
--- app/src/main/java/com/malainho/t1/ui/main/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.malainho.t1.ui.main
-
-import androidx.biometric.BiometricPrompt
-import androidx.lifecycle.ViewModel
-import com.malainho.t1.extensions.promptBiometricChecker
-
-class MainViewModel : ViewModel() {
-
-}
